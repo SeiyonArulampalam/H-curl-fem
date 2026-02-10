030158069dacbc6e65a123447a57058b8e383d19
Seiyon Arulampalam <sarulampalam3@gatech.edu>
2026-02-10 15:28:41 -0500

    Model works with the enforcement of specific boundary conditions.

--- interp.py
+++ interp.py
@@ -2,10 +2,47 @@ import numpy as np
 import matplotlib.tri as tri
 import matplotlib.pyplot as plt
 
-np.set_printoptions(precision=2)
+np.set_printoptions(precision=2, linewidth=200)
 
 
-def TriangleGuassianQuadrature(n1_coord, n2_coord, n3_coord, idx, idy):
+def get_node_coords(element, edge_conn, elem_edge_conn, X):
+    # Extract the edges
+    E1 = elem_edge_conn[element][0]
+    E2 = elem_edge_conn[element][1]
+    E3 = elem_edge_conn[element][2]
+
+    nodes_edge1 = edge_conn[E1]
+    nodes_edge2 = edge_conn[E2]
+    nodes_edge3 = edge_conn[E3]
+
+    # Comine the edges based on the elem_edge_conn
+    nodes = [
+        nodes_edge1[0],
+        nodes_edge1[1],
+        nodes_edge2[0],
+        nodes_edge2[1],
+        nodes_edge3[0],
+        nodes_edge3[1],
+    ]
+
+    # Extract the unique node tags in the order of the edge connectivity
+    n1_tag = nodes_edge1[0]
+    n2_tag = nodes_edge2[0]
+    n3_tag = nodes_edge3[0]
+
+    # Extract the coordinates
+    n1_coord = X[n1_tag]
+    n2_coord = X[n2_tag]
+    n3_coord = X[n3_tag]
+    return n1_coord, n2_coord, n3_coord
+
+
+def TriangleGuassianQuadrature(element, edge_conn, elem_edge_conn, X, idx, idy):
+    # Get the node coordinates for the element.
+    n1_coord, n2_coord, n3_coord = get_node_coords(
+        element, edge_conn, elem_edge_conn, X
+    )
+
     pts = np.array([[1 / 3, 1 / 3, 1 / 3]])
     wts = np.array([1.0])
     I = 0.0
@@ -70,6 +107,7 @@ def eval_shape_funcs(n1_coord, n2_coord, n3_coord, x, y):
 
     # Area of an element
     area = 0.5 * (b1 * c2 - b2 * c1)
+    assert area > 0
 
     # Compute the length of each edge
     length = lambda x1, y1, x2, y2: np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
@@ -138,7 +176,12 @@ def barycenter_sampling(n1_coord, n2_coord, n3_coord):
     return new_point[0], new_point[1]  # xi and eta frame
 
 
-def visualize_element_basis(n1_coord, n2_coord, n3_coord):
+def visualize_element_basis(element, edge_conn, elem_edge_conn, X, title=None):
+    # Get the node coordinates for the element.
+    n1_coord, n2_coord, n3_coord = get_node_coords(
+        element, edge_conn, elem_edge_conn, X
+    )
+
     # Initialize plot
     fig, ax = plt.subplots(ncols=3)
 
@@ -174,21 +217,36 @@ def visualize_element_basis(n1_coord, n2_coord, n3_coord):
     ax[0].set_title(r"$N_1$")
     ax[1].set_title(r"$N_2$")
     ax[2].set_title(r"$N_3$")
+    fig.suptitle(title)
+    plt.savefig(title + ".jpg", dpi=800)
     return
 
 
-def plot_element_solution(n1_coord, n2_coord, n3_coord, soln, fix, ax):
+def plot_element_solution(
+    element,
+    edge_conn,
+    elem_edge_conn,
+    X,
+    soln,
+    fix,
+    ax,
+    signs=[1.0, 1.0, 1.0],
+):
+    # Get the node coordinates for the element.
+    n1_coord, n2_coord, n3_coord = get_node_coords(
+        element, edge_conn, elem_edge_conn, X
+    )
 
-    # Plot the mesh
+    # Plot the element mesh
     triang = tri.Triangulation(
         [n1_coord[0], n2_coord[0], n3_coord[0]],
         [n1_coord[1], n2_coord[1], n3_coord[1]],
         [[0, 1, 2]],
     )
-    ax.triplot(triang, "bo--")
+    ax.triplot(triang, color="grey", linestyle="-", linewidth=1.0)
 
     # Compute the vector field inside the element
-    npts = 50  # total number of points in the vector field
+    npts = 150  # total number of points in the vector field
     for i in range(npts):
         # Get the x, y point in the triangle to evaluate the shape function
         x, y = barycenter_sampling(n1_coord, n2_coord, n3_coord)
@@ -198,10 +256,46 @@ def plot_element_solution(n1_coord, n2_coord, n3_coord, soln, fix, ax):
             n1_coord, n2_coord, n3_coord, x, y
         )
 
-        E = N1 * soln[0] + N2 * soln[1] + N3 * soln[2]
+        E = signs[0] * N1 * soln[0] + signs[1] * N2 * soln[1] + signs[2] * N3 * soln[2]
 
         # Update the plot
         ax.quiver(x, y, E[0], E[1], width=4e-3)
+
+    ax.set_aspect("equal")
+    return
+
+
+def plot_mesh(X, elem_conn, edge_conn, elem_edge_conn):
+    fig, ax = plt.subplots()
+    triang = tri.Triangulation(X[:, 0], X[:, 1], elem_conn)
+    ax.triplot(triang, color="grey", linestyle="--", linewidth=1.0)
+
+    # Plot the node tags for each element
+    for e in range(len(elem_conn)):
+        elem_node_tags = elem_conn[e]
+        n1_tag = elem_node_tags[0]
+        n2_tag = elem_node_tags[1]
+        n3_tag = elem_node_tags[2]
+        plt.text(X[n1_tag, 0], X[n1_tag, 1], f"{n1_tag}")
+        plt.text(X[n2_tag, 0], X[n2_tag, 1], f"{n2_tag}")
+        plt.text(X[n3_tag, 0], X[n3_tag, 1], f"{n3_tag}")
+
+    # Plot the edges for each element
+    for e in range(len(elem_conn)):
+        edge_tags = elem_edge_conn[e]
+
+        for edge_i in edge_tags:
+            n1_tag = edge_conn[edge_i][0]  # Start node for edge
+            n2_tag = edge_conn[edge_i][1]  # End node for edge
+            x1 = X[n1_tag, 0]
+            y1 = X[n1_tag, 1]
+            x2 = X[n2_tag, 0]
+            y2 = X[n2_tag, 1]
+            xm = 0.5 * (x1 + x2)
+            ym = 0.5 * (y1 + y2)
+            plt.text(xm, ym, f"\nE{edge_i}")
+
+    plt.savefig("mesh.jpg", dpi=1000)
 
     return
 
@@ -210,63 +304,62 @@ if __name__ == "__main__":
     # coords
     X = np.array(
         [
-            [-1.0, 0.0],
-            [1.0, -0.8],
-            [-0.5, 1.5],
-            [1.0, 2.0],
-            [3.0, 0.5],
+            [0.0, 0.0],  # node 0
+            [1.0, 0.0],  # node 1
+            [0.0, 1.0],  # node 2
+            [1.0, 1.0],  # node 3
+            [2.0, 1.0],  # node 4
         ]
     )
 
     # elem -> node
     elem_conn = [
-        [0, 1, 2],
-        [3, 2, 1],
-        [1, 4, 3],
+        [1, 2, 0],  # e0
+        [3, 2, 1],  # e1
+        [1, 4, 3],  # e2
     ]
 
     edge_conn = [
         [0, 1],  # E0
         [1, 2],  # E1
         [2, 0],  # E2
-        [3, 2],  # E3
-        [2, 1],  # E4
-        [1, 3],  # E5
+        [1, 3],  # E3
+        [3, 2],  # E4
+        [2, 1],  # E5
         [3, 1],  # E6
         [1, 4],  # E7
         [4, 3],  # E8
     ]
 
-    #! WHYYYYYYYYYYYYYYYYYYYYY
     elem_edge_conn = [
         [2, 0, 1],
-        [3, 4, 5],
+        [5, 3, 4],
         [7, 8, 6],
     ]
+
+    # Plot the mesh and the connectivity
+    plot_mesh(X, elem_conn, edge_conn, elem_edge_conn)
 
     # Assemble stiffness matrix
     K = np.zeros((9, 9))
     # Loop through each element
     for e in range(len(elem_conn)):
-        # Extract the node tags
-        n1_tag = elem_conn[e][0]
-        n2_tag = elem_conn[e][1]
-        n3_tag = elem_conn[e][2]
-
-        # Extract the node coordinates
-        n1_coord = X[n1_tag]
-        n2_coord = X[n2_tag]
-        n3_coord = X[n3_tag]
-
-        # visualize_element_basis(n1_coord, n2_coord, n3_coord)
+        visualize_element_basis(
+            e,
+            edge_conn,
+            elem_edge_conn,
+            X,
+            title=f"Element {e} H(curl)",
+        )
 
         for i in range(3):
             row = elem_edge_conn[e][i]
             for j in range(3):
                 col = elem_edge_conn[e][j]
                 K[row, col] += TriangleGuassianQuadrature(
-                    n1_coord, n2_coord, n3_coord, i, j
+                    e, edge_conn, elem_edge_conn, X, i, j
                 )
+    print("\nStiffness Matrix (No BCs)")
     print(K)
 
     # Define G
@@ -283,51 +376,61 @@ if __name__ == "__main__":
     E[0:9, [-1, -2]] = G.T
     E[[-1, -2], 0:9] = G
     rhs = np.zeros(11)
+
+    print("\nMatrix E w/ Lagrange Multipliers")
     print(E)
 
     # Apply boundary condition to E
     # bc = 0
     # E[bc, :] = 0.0
     # E[bc, bc] = 1.0
-    # rhs[bc] = 1.0
+    # rhs[bc] = 0.0
 
-    # E[2, :] = 0.0
-    # E[2, 2] = 1.0
-    # rhs[2] = 1.0
+    # E[0, :] = 0.0
+    # E[0, 0] = 1.0
+    # rhs[0] = 1.0
 
-    # E[4, :] = 0.0
-    # E[4, 4] = 1.0
-    # rhs[4] = 0.0
+    E[2, :] = 0.0
+    E[2, 2] = 1.0
+    rhs[2] = 1.0
+
+    E[4, :] = 0.0
+    E[4, 4] = 1.0
+    rhs[4] = 1.0
+
+    # E[7, :] = 0.0
+    # E[7, 7] = 1.0
+    # rhs[7] = 0.0
 
     E[8, :] = 0.0
     E[8, 8] = 1.0
     rhs[8] = 1.0
 
+    print("\nMatrix E w/ BCs")
     print(E)
+
+    print("\n RHS Vector")
     print(rhs)
+    print()
     u = np.linalg.solve(E, rhs)
 
     for i, soln_i in enumerate(u):
         print(f"u[{i}] = {soln_i:4f}")
 
     # Initialize plot
+    print()
     fig1, ax1 = plt.subplots()
-    for e in range(len(elem_conn)):
-        # Extract the node tags
-        n1_tag = elem_conn[e][0]
-        n2_tag = elem_conn[e][1]
-        n3_tag = elem_conn[e][2]
-
-        # Extract the node coordinates
-        n1_coord = X[n1_tag]
-        n2_coord = X[n2_tag]
-        n3_coord = X[n3_tag]
-
-        # Extract solution
-        soln = u[elem_edge_conn[e]]
-        print(f"Element: {e}, Edges{elem_edge_conn[e]}, soln: {soln}")
-
-        plot_element_solution(n1_coord, n2_coord, n3_coord, soln, fig1, ax1)
-
+    soln1 = u[elem_edge_conn[0]]
+    soln2 = u[elem_edge_conn[1]]
+    soln3 = u[elem_edge_conn[2]]
+    plot_element_solution(
+        0, edge_conn, elem_edge_conn, X, soln1, fig1, ax1, signs=[1, 1, 1]
+    )
+    plot_element_solution(
+        1, edge_conn, elem_edge_conn, X, soln2, fig1, ax1, signs=[1, 1, 1]
+    )
+    plot_element_solution(
+        2, edge_conn, elem_edge_conn, X, soln3, fig1, ax1, signs=[1, 1, 1]
+    )
     plt.savefig("fem.jpg", dpi=800)
     # plt.show()
